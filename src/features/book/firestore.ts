7834db85a1ac0b530dd127c83a1e1c0f60c7171b
kagan <intend.on.games+git@gmail.com>
2023-05-21 16:29:52 +0900

    不要記述を削除

--- src/features/book/firestore.ts
+++ src/features/book/firestore.ts
@@ -7,7 +7,7 @@ export const bookConverter: FirestoreDataConverter<Book> = {
   },
 
   fromFirestore(snapshot, options) {
-    const data = snapshot.data(options) as Omit<Book, "id">;
+    const data = snapshot.data(options) as Book;
     return {
       id: snapshot.id,
       ...data,
