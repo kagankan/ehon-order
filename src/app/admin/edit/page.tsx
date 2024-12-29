c99dfe0560cafca2a4aa0a2e09591a7aee95b7da
kagan <intend.on.games+git@gmail.com>
2024-12-29 21:22:06 +0900

    fix: エラー解消

--- src/app/admin/edit/page.tsx
+++ src/app/admin/edit/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import Edit from "../_components/view/edit";
 
 export default function Page() {
-  return <Edit />;
+  return (
+    <Suspense fallback={<div>読込中...</div>}>
+      <Edit />
+    </Suspense>
+  );
 }
