384a6fafa32719833f3987bea2f1d2e48d0bf9b1
kagan <intend.on.games+git@gmail.com>
2025-01-19 17:17:03 +0900

    chore: compatが不要な設定を整理

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import _import from "eslint-plugin-import";
-import boundaries from "eslint-plugin-boundaries";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import configPrettier from "eslint-config-prettier";
+import boundaries from "eslint-plugin-boundaries";
+import importPlugin from "eslint-plugin-import";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -21,120 +23,80 @@ export default [
   {
     ignores: ["!**/*.js"],
   },
-  ...fixupConfigRules(
-    compat.extends(
-      "next/core-web-vitals",
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      "plugin:react/recommended",
-      "plugin:react-hooks/recommended",
-      "plugin:jsx-a11y/recommended",
-      "prettier"
-    )
-  ),
+  js.configs.recommended,
+  ...fixupConfigRules(compat.extends("next/core-web-vitals")),
   {
     plugins: {
-      import: fixupPluginRules(_import),
+      import: fixupPluginRules(importPlugin),
       boundaries,
     },
-
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
       },
     },
-
     settings: {
       "boundaries/elements": [
-        {
-          type: "domain",
-          pattern: "src/domain",
-        },
-        {
-          type: "infrastructure",
-          pattern: "src/infrastructure",
-        },
-        {
-          type: "use-case",
-          pattern: "src/use-case",
-        },
-        {
-          type: "app",
-          pattern: "src/app",
-        },
-        {
-          type: "di",
-          pattern: "src/di",
-        },
+        { type: "domain", pattern: "src/domain" },
+        { type: "infrastructure", pattern: "src/infrastructure" },
+        { type: "use-case", pattern: "src/use-case" },
+        { type: "app", pattern: "src/app" },
+        { type: "di", pattern: "src/di" },
       ],
     },
-
     rules: {
       "no-console": "warn",
-
       "import/order": [
         "warn",
         {
-          alphabetize: {
-            order: "asc",
-            caseInsensitive: true,
-          },
-
+          alphabetize: { order: "asc", caseInsensitive: true },
           "newlines-between": "never",
-
           pathGroups: [
-            {
-              pattern: "@/**",
-              group: "parent",
-              position: "before",
-            },
+            { pattern: "@/**", group: "parent", position: "before" },
           ],
         },
       ],
-
       "react/react-in-jsx-scope": "off",
       "@next/next/no-img-element": "off",
-
       "boundaries/element-types": [
         "error",
         {
           default: "disallow",
-
           rules: [
-            {
-              from: "infrastructure",
-              allow: [["domain"]],
-            },
-            {
-              from: "use-case",
-              allow: [["domain"]],
-            },
-            {
-              from: "app",
-              allow: [["domain"], ["use-case"], ["di"]],
-            },
-            {
-              from: "di",
-              allow: [["infrastructure"], ["use-case"]],
-            },
+            { from: "infrastructure", allow: [["domain"]] },
+            { from: "use-case", allow: [["domain"]] },
+            { from: "app", allow: [["domain"], ["use-case"], ["di"]] },
+            { from: "di", allow: [["infrastructure"], ["use-case"]] },
           ],
         },
       ],
     },
   },
+  ...[
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    // 別途追加したい
+    // ...tseslint.configs.stylistic,
+    // ...tseslint.configs.stylisticTypeChecked,
+  ].map((config) => ({
+    ...config,
+    files: ["**/*.ts", "**/*.tsx"],
+  })),
   {
     files: ["**/*.ts", "**/*.tsx"],
-
     languageOptions: {
       parser: tsParser,
       ecmaVersion: 5,
       sourceType: "module",
-
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
+      parserOptions: { project: "./tsconfig.json" },
     },
   },
+  {
+    files: ["*"],
+    rules: {
+      "import/no-anonymous-default-export": "off",
+    },
+  },
+  configPrettier,
 ];
